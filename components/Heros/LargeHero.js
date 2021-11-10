21c73ba21b3f00a547f61bb370b498d24f63598c
Chris Barnes <barnes.chris@gmail.com>
2021-11-09 20:46:02 -0500

    Fix large hero and optimize image

--- components/Heros/LargeHero.js
+++ components/Heros/LargeHero.js
@@ -8,12 +8,15 @@ const LargeHero = ({ heading, description, link, backgroundImage }) => {
         src={backgroundImage}
         layout="fill"
         objectFit="cover"
-        style={{ zIndex: "-1" }}
+        className="z-0"
+        priority
       />
-      <h1>{heading}</h1>
-      {description && <p>{description}</p>}
+      <h1 className="relative z-10">{heading}</h1>
+      {description && <p className="relative z-10">{description}</p>}
       {link && link.url && link.text && (
-        <Link href={link.url}>{link.text}</Link>
+        <Link href={link.url}>
+          <a className="relative z-10">{link.text}</a>
+        </Link>
       )}
     </div>
   );
