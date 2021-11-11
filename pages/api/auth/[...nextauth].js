6a4c30571acb2b53f1cc9cb6ea3f12b073da0f76
Chris Barnes <barnes.chris@gmail.com>
2021-11-11 13:55:04 -0500

    Update auth0 provider settings

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,5 +2,11 @@ import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/Auth0";
 
 export default NextAuth({
-  providers: [Auth0Provider({})],
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
 });
