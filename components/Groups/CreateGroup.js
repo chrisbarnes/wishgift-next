70562fb77c27e66dd82976f00679cc56fbc8d900
Chris Barnes <barnes.chris@gmail.com>
2021-11-15 15:24:57 -0500

    Update CreateGroup component to send data

--- components/Groups/CreateGroup.js
+++ components/Groups/CreateGroup.js
@@ -8,10 +8,18 @@ const CreateGroup = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const { data, error } = await fetch("/api/groups/create", {
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
+
+    if (error) return "An error has occurred.";
+    if (!data) return "Loading..."; // todo create a better loading component here
+  };
 
   return (
-    <div className="px-6 py-4 shadow-md rounded-md">
+    <div className="px-6 py-4 shadow-md rounded-md mb-8">
       <form onSubmit={handleSubmit(onSubmit)}>
         <TextInputControl
           id="name"
