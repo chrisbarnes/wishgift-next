bf92cf6c1707c9063bcdf2c7844a51394945a9d9
Chris Barnes <barnes.chris@gmail.com>
2021-11-15 15:51:06 -0500

    Add submitting state to button/form

--- components/Groups/CreateGroup.js
+++ components/Groups/CreateGroup.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import TextInputControl from "../Forms/TextInputControl";
 import Button from "../Forms/Button";
 
 const CreateGroup = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
-  const onSubmit = async (data) => {
-    const { data: res, error } = await fetch("/api/groups/create", {
+  const submitData = async (data) => {
+    setIsSubmitting(true);
+    const response = await fetch("/api/groups/create", {
       method: "PUT",
       body: JSON.stringify(data),
+    });
+    setIsSubmitting(false);
+    return response.json();
+  };
+  const onSubmit = async (data) => {
+    submitData(data).then(({ data }) => {
+      // If the form was submitted successfully, reset it so we can submit another
+      if (data.message === "Success") {
+        reset({});
+      }
     });
   };
 
@@ -33,7 +47,7 @@ const CreateGroup = () => {
           required
         />
 
-        <Button type="submit">Create Group</Button>
+        <Button type="submit">Create Group {isSubmitting && "..."}</Button>
       </form>
     </div>
   );
