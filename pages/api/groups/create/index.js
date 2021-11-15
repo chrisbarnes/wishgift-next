6d49c582730d74eab5581302bb720153e6e6cbdc
Chris Barnes <barnes.chris@gmail.com>
2021-11-15 15:24:39 -0500

    Add CreateGroup endpoint

---
+++ pages/api/groups/create/index.js
@@ -0,0 +1,27 @@
+import { getSession } from "next-auth/react";
+import { query as q } from "faunadb";
+import { faunaClient } from "../../../lib/fauna";
+
+export default async function createGroup(req, res) {
+  const session = await getSession({ req });
+
+  if (session && req.method === "PUT") {
+    try {
+      const data = JSON.parse(req.body);
+
+      const query = await faunaClient.query(
+        q.Create(q.Collection("groups"), {
+          data: { title: data.title, description: data.description },
+        })
+      );
+
+      res.status(200).json({ data: { data: query, message: "Success" } });
+    } catch (error) {
+      res.status(500);
+    }
+  } else {
+    res.status(401);
+  }
+
+  res.end();
+}
