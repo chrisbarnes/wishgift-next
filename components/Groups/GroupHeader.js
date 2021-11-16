4dab59cd10da1d234b70c2dc103f078f05b7ab72
Chris Barnes <barnes.chris@gmail.com>
2021-11-15 20:47:53 -0500

    Basic group header styling

--- components/Groups/GroupHeader.js
+++ components/Groups/GroupHeader.js
@@ -3,8 +3,8 @@ import React from "react";
 const GroupHeader = ({ name, description }) => {
   return (
     <div>
-      <h1>{name}</h1>
-      <p>{description}</p>
+      <h1 className="text-xl font-extrabold mb-2 text-gray-700">{name}</h1>
+      <p className="text-sm text-black">{description}</p>
     </div>
   );
 };
