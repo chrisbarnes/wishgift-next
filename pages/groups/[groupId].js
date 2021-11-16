4f48759d0820352685ef5753326b1bf5ccd151b9
Chris Barnes <barnes.chris@gmail.com>
2021-11-15 20:48:23 -0500

    Update group page to get group data

--- pages/groups/[groupId].js
+++ pages/groups/[groupId].js
@@ -1,16 +1,34 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
 import GroupHeader from "../../components/Groups/GroupHeader";
 import GiftsList from "../../components/Gifts/GiftsList";
 
-const GroupPage = ({ groupName, groupDescription }) => {
-  const { query } = useRouter();
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-  return (
-    <>
-      <GroupHeader name={groupName} description={groupDescription} />
-      <GiftsList groupId={query.groupId} />
-    </>
+const GroupPage = (props) => {
+  const { query } = useRouter();
+  const { data: groupData, groupError } = useSWR(
+    `/api/groups/${query.groupId}`,
+    fetcher
   );
+
+  if (groupError) {
+    return <p>Sorry. There was an error retrieving this group.</p>;
+  }
+
+  if (groupData) {
+    return (
+      <>
+        <GroupHeader
+          name={groupData.name}
+          description={groupData.description}
+        />
+        <GiftsList groupId={query.groupId} />
+      </>
+    );
+  }
+
+  return <p>No group data yet.</p>;
 };
 
 export default GroupPage;
