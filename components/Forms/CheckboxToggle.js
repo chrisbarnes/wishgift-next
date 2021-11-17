a881e489c27576a9284bbbe737b8a2296dc0db4b
Chris Barnes <barnes.chris@gmail.com>
2021-11-16 19:43:05 -0500

    Update color to match button

--- components/Forms/CheckboxToggle.js
+++ components/Forms/CheckboxToggle.js
@@ -9,7 +9,7 @@ const CheckboxToggle = ({ label, isChecked }) => {
         class="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
         checked={isChecked}
       />
-      <span class="w-10 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-4"></span>
+      <span class="w-10 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-blue-700 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-4"></span>
     </label>
   );
 };
