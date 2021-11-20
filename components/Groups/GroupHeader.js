c3c50c9287382ceffc17d234f6e37e0a2b5e43f2
Chris Barnes <barnes.chris@gmail.com>
2021-11-19 19:05:44 -0500

    Random border color for the group header

--- components/Groups/GroupHeader.js
+++ components/Groups/GroupHeader.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const GroupHeader = ({ name, description }) => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
   return (
-    <div className="mb-12">
+    <div
+      className="mb-12 pb-4 border-b-4"
+      style={{ borderColor: `#${randomColor}` }}
+    >
       <h1 className="text-4xl font-extrabold mb-2 text-gray-700">{name}</h1>
       <p className="text-sm text-black">{description}</p>
     </div>
