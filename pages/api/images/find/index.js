3b1cfe771d9081f6d4eb62eaf149380d830f2128
Chris Barnes <barnes.chris@gmail.com>
2021-11-22 12:50:37 -0500

    Safely check for images before returning a response

--- pages/api/images/find/index.js
+++ pages/api/images/find/index.js
@@ -1,8 +1,7 @@
 import { getSession } from "next-auth/react";
 
-// const isProd = process.env.IS_PROD;
 const endpoint = process.env.GIFT_IMAGES_ENDPOINT;
-const numImages = process.env.GIFT_IMAGES_NUM_RETURNED || 10;
+const numImages = process.env.GIFT_IMAGES_NUM_RETURNED || 12;
 
 export default async function getGiftImages(req, res) {
   const session = await getSession({ req });
@@ -14,7 +13,13 @@ export default async function getGiftImages(req, res) {
     const response = await fetch(`${endpoint}?url=${url}`);
     const images = await response.json();
 
-    res.status(200).json({ data: { images: images.slice(0, numImages) } });
+    if (images && images.length) {
+      res.status(200).json({ data: { images: images.slice(0, numImages) } });
+    } else {
+      console.log(`No images found for ${url}`);
+
+      res.status(200).json({ data: { images: [] } });
+    }
   } else {
     res.status(401);
   }
