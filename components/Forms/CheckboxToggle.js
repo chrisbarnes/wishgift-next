16829e14c4f1ec6c9da7946a65aaf1095a157fa5
Chris Barnes <barnes.chris@gmail.com>
2021-11-28 21:49:04 -0500

    Update the colors of the toggle

--- components/Forms/CheckboxToggle.js
+++ components/Forms/CheckboxToggle.js
@@ -8,7 +8,7 @@ const CheckboxToggle = ({ label, isChecked, onChange }) => {
         defaultChecked={isChecked}
         onChange={onChange}
       />
-      <span className="w-10 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-200 rounded-full duration-200 ease-in-out peer-checked:bg-white after:w-4 after:h-4 after:bg-blue-700 after:rounded-full after:shadow-md after:duration-200 peer-checked:after:translate-x-4"></span>
+      <span className="w-10 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-200 rounded-full duration-200 ease-in-out peer-checked:bg-white after:w-4 after:h-4 peer-checked:after:bg-gray-700 after:bg-blue-700 after:rounded-full after:shadow-md after:duration-200 peer-checked:after:translate-x-4"></span>
     </label>
   );
 };
