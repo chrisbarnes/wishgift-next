af0968d43d54617fb3576152cea510ecf32a48e3
Chris Barnes <barnes.chris@gmail.com>
2021-11-30 16:57:39 -0500

    Update tailwind config so colors classes aren't purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      "bg-blue-700",
+      "bg-red-700",
+      "bg-pink-700",
+      "bg-yellow-700",
+      "bg-green-700",
+      "bg-purple-700",
+      "border-blue-700",
+      "border-red-700",
+      "border-pink-700",
+      "border-yellow-700",
+      "border-green-700",
+      "border-purple-700",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
