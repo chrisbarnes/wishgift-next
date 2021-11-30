efa71d311f3a662f96cbc413f8fc19667e10e623
Chris Barnes <barnes.chris@gmail.com>
2021-11-29 22:17:10 -0500

    Horizontal form stylings

--- components/Forms/TextInput.js
+++ components/Forms/TextInput.js
@@ -1,13 +1,23 @@
 import Label from "./Label";
 
-const TextInput = ({ id, label, required, register, value, onChange }) => {
+const TextInput = ({
+  id,
+  label,
+  placeholder,
+  required,
+  register,
+  value,
+  onChange,
+}) => {
   return (
     <>
-      <Label id={id} label={label} />
+      {label && <Label id={id} label={label} />}
       <input
         {...register(id, { required, onChange })}
+        id={id}
+        placeholder={placeholder}
         defaultValue={value}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 max-w-xs"
       />
     </>
   );
--- components/Forms/TextInputControl.js
+++ components/Forms/TextInputControl.js
@@ -1,24 +1,35 @@
+import clsx from "clsx";
 import TextInput from "./TextInput";
 import Error from "./Error";
 
 const TextInputControl = ({
   id,
   label,
+  placeholder,
   required,
   errors,
   register,
   value,
   onChange,
+  horizontal,
 }) => {
+  const wrapperClasses = clsx({
+    "mb-2": !horizontal,
+    flex: horizontal,
+    "items-center": horizontal,
+    "flex-grow": horizontal,
+  });
   return (
-    <div className="mb-2">
+    <div className={wrapperClasses}>
       <TextInput
         id={id}
         label={label}
+        placeholder={placeholder}
         register={register}
         required={required}
         value={value}
         onChange={onChange}
+        horizontal={horizontal}
       />
       <Error errors={errors} name={id} />
     </div>
