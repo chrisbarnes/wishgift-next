688b514f928db818a2f1a694051dec01c2550583
Chris Barnes <barnes.chris@gmail.com>
2021-11-29 22:39:27 -0500

    Update language displayed for 0 gifts found

--- components/Gifts/GiftsCount.js
+++ components/Gifts/GiftsCount.js
@@ -1,6 +1,10 @@
 const GiftsCount = ({ totalGifts, filteredGifts }) => {
+  if (filteredGifts === 0) {
+    return <p className="text-2xl font-light ">No gifts found.</p>;
+  }
+
   return (
-    <p className="text-2xl font-light ">
+    <p className="text-2xl font-light">
       Showing {totalGifts > 1 && filteredGifts === totalGifts && "all "}
       <strong className="font-extrabold">{filteredGifts}</strong> gift
       {filteredGifts > 1 && "s"}.
