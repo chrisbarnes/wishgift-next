d542d2914da3ad23dcaaa41ac23d5630afec62bc
Chris Barnes <barnes.chris@gmail.com>
2021-11-29 22:15:36 -0500

    Add the search form component

---
+++ components/Search/SearchForm.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import clsx from "clsx";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+import TextInputControl from "../Forms/TextInputControl";
+import Button from "../Forms/Button";
+import Icons from "../Icons";
+import AccessibleText from "../Utils/AccessibleText";
+
+const SearchForm = ({ searchCallback, resetFilters }) => {
+  const isWidePage = useMediaQuery("(min-width: 768px)");
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, dirtyFields },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // Call the callback and pass the data
+    searchCallback(data);
+  };
+
+  const onReset = () => {
+    reset();
+    resetFilters();
+  };
+
+  const toggleSearch = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const wrapperClasses = clsx({
+    "py-4": true,
+    flex: true,
+    "md:translate-x-80": !isExpanded,
+    "transition-transform": true,
+  });
+
+  return (
+    <div className="md:overflow-hidden">
+      <div className={wrapperClasses}>
+        <button className="mr-2 text-gray-700" onClick={toggleSearch}>
+          <Icons.Search size="lg" />
+          <AccessibleText>Toggle Search</AccessibleText>
+        </button>
+        <form
+          className="flex items-center w-54 md:w-80"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="relative flex-grow pr-2 md:pr-0">
+            <TextInputControl
+              id="search"
+              placeholder="Search gifts"
+              register={register}
+              errors={errors}
+              required
+              onChange={
+                isWidePage
+                  ? (e) => searchCallback({ search: e.target.value })
+                  : null
+              }
+              horizontal
+            />
+            {dirtyFields && dirtyFields.search && (
+              <button
+                className="absolute right-4 top-2"
+                type="button"
+                onClick={onReset}
+              >
+                <Icons.Close size="sm" />
+                <AccessibleText>Clear Search</AccessibleText>
+              </button>
+            )}
+          </div>
+          <Button className="md:hidden">Search</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
