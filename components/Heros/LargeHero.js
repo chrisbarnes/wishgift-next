52b196474540b7de6123369626a004ac8818f62f
Chris Barnes <barnes.chris@gmail.com>
2022-11-27 13:24:59 -0500

    Next and React upgrades

--- components/Heros/LargeHero.js
+++ components/Heros/LargeHero.js
@@ -7,20 +7,15 @@ const LargeHero = ({ heading, description, link, backgroundImage }) => {
       className="relative flex flex-col justify-center items-center rounded-md overflow-hidden text-white"
       style={{ height: "66.66vw" }}
     >
-      <Image
-        src={backgroundImage}
-        layout="fill"
-        objectFit="cover"
-        className="z-0 max-w-full"
-        priority
-      />
+      <Image src={backgroundImage} fill className="z-0 max-w-full" priority />
       <h1 className="relative z-10 font-bold text-6xl">{heading}</h1>
       {description && <p className="relative z-10 mb-8">{description}</p>}
       {link && link.url && link.text && (
-        <Link href={link.url}>
-          <a className="relative z-10 rounded-md bg-blue-700 text-white px-4 py-2">
-            {link.text}
-          </a>
+        <Link
+          href={link.url}
+          className="relative z-10 rounded-md bg-blue-700 text-white px-4 py-2"
+        >
+          {link.text}
         </Link>
       )}
     </div>
