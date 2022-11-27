3864c06a563b918f69451df0e267a296c2e345d6
Chris Barnes <barnes.chris@gmail.com>
2022-11-27 13:26:07 -0500

    Add empty alt attribute for a11y

--- components/Heros/LargeHero.js
+++ components/Heros/LargeHero.js
@@ -7,7 +7,13 @@ const LargeHero = ({ heading, description, link, backgroundImage }) => {
       className="relative flex flex-col justify-center items-center rounded-md overflow-hidden text-white"
       style={{ height: "66.66vw" }}
     >
-      <Image src={backgroundImage} fill className="z-0 max-w-full" priority />
+      <Image
+        src={backgroundImage}
+        fill
+        className="z-0 max-w-full"
+        priority
+        alt=""
+      />
       <h1 className="relative z-10 font-bold text-6xl">{heading}</h1>
       {description && <p className="relative z-10 mb-8">{description}</p>}
       {link && link.url && link.text && (
